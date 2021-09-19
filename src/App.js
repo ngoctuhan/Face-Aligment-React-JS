e662512afb18a708c8145b0cf8bf150fed70ab29
ngoctuhan <hoangmautrung@gmail.com>
2021-09-19 11:36:01 +0700

    check webcam

--- src/App.js
+++ src/App.js
@@ -3,17 +3,73 @@ import './App.css';
 import Webcam from 'react-webcam';
 import * as tf from '@tensorflow/tfjs';
 import * as facemesh from '@tensorflow-models/face-landmarks-detection';
-import { drawMesh } from "./utils";
+import {NotificationContainer, NotificationManager} from 'react-notifications';
+import { check_angel_face, check_mouth_activate } from "./utils";
 function App() {
 
   // save state/value of element
   const webcamRef = useRef(null);
   const canvasRef = useRef(null);
 
+
+  const [capturing, setCapturing] = React.useState(false);
+  const [recordedChunks, setRecordedChunks] = React.useState([]);
+  const mediaRecorderRef = React.useRef(null);
+
+  let count_empty = 0;
+  let count_frame = 0;
+  let count_cheat = 0;
+  let count_over  = 0;
+  let status_recording = 0;
+
+  const handleStartCaptureClick = React.useCallback(() => {
+    console.log("dang quay lại video");
+    setCapturing(true);
+    mediaRecorderRef.current = new MediaRecorder(webcamRef.current.stream, {
+      mimeType: "video/webm"
+    });
+    mediaRecorderRef.current.addEventListener(
+      "dataavailable",
+      handleDataAvailable
+    );
+    mediaRecorderRef.current.start();
+  }, [webcamRef, setCapturing, mediaRecorderRef]);
+
+  const handleDataAvailable = React.useCallback(
+    ({ data }) => {
+      if (data.size > 0) {
+        setRecordedChunks((prev) => prev.concat(data));
+      }
+    },
+    [setRecordedChunks]
+  );
+
+  const handleStopCaptureClick = React.useCallback(() => {
+    console.log('đang tắt quay video');
+    mediaRecorderRef.current.stop();
+    setCapturing(false);
+  }, [mediaRecorderRef, webcamRef, setCapturing]);
+
+  const handleDownload = React.useCallback(() => {
+    if (recordedChunks.length) {
+      const blob = new Blob(recordedChunks, {
+        type: "video/webm"
+      });
+      const url = URL.createObjectURL(blob);
+      const a = document.createElement("a");
+      document.body.appendChild(a);
+      a.style = "display: block";
+      a.href = url;
+      a.download = "react-webcam-stream-capture.webm";
+      a.click();
+      window.URL.revokeObjectURL(url);
+      setRecordedChunks([]);
+    }
+  }, [recordedChunks]);
+
   //  Load posenet
   const runFacemesh = async () => {
    
-    // NEW MODEL
     const net = await facemesh.load(facemesh.SupportedPackages.mediapipeFacemesh);
     setInterval(() => {
       detect(net);
@@ -26,26 +82,86 @@ function App() {
       webcamRef.current !== null &&
       webcamRef.current.video.readyState === 4
     ) {
+      
       // Get Video Properties
       const video = webcamRef.current.video;
-      const videoWidth = webcamRef.current.video.videoWidth;
-      const videoHeight = webcamRef.current.video.videoHeight;
-
-      // Set video width
-      webcamRef.current.video.width = videoWidth;
-      webcamRef.current.video.height = videoHeight;
-
-      // Set canvas width
-      canvasRef.current.width = videoWidth;
-      canvasRef.current.height = videoHeight;
-
      
-      const face = await net.estimateFaces({input:video});
-      console.log(face);
+      count_frame = count_frame + 1;
+      // console.log(count_frame);
+      if (count_frame % 20 === 0)
+      {
+        // 1s process 2-3 times
+        const face = await net.estimateFaces({input:video});
 
-      // Get canvas context
-      const ctx = canvasRef.current.getContext("2d");
-      requestAnimationFrame(()=>{drawMesh(face, ctx)});
+        if(face.length === 0)
+        {
+          // Dont have anyone
+          
+          count_empty = count_empty + 1
+          if (count_empty > 7) 
+          // keep tracking can not see person after times (7s) must send to sever a warning
+          // 
+          {
+            // send to sever warning. 
+            console.log('khong co ai trong dang thi ca');
+            count_empty = 0;
+            count_over = 0;
+            count_cheat = 0;
+          }
+        }
+        else if (face.length === 1)
+        {
+          // Process image to cal angle 
+
+          let circumstance =  check_angel_face(face);
+
+          let is_opened = check_mouth_activate(face);
+
+          console.log("Góc khuôn mặt là: "+circumstance);
+          
+          if (circumstance > 0 || is_opened > 0){
+
+            if (count_cheat === 0){
+              status_recording = 1;
+              handleStartCaptureClick();
+            }
+            count_cheat = count_cheat + 1;
+          }
+        }
+        else
+        {
+          // Over person can accept
+          count_over =  count_over + 1;
+
+          if (count_over > 3)
+          {
+            console.log('Có nhiều người trong phòng thi');
+            count_over  = 0;
+            count_empty = 0;
+            count_cheat = 0;
+            // send to sever warning 
+          }
+        }
+
+        if (count_cheat == 0 && status_recording == 1){
+          handleStopCaptureClick();
+          status_recording = 0;
+        }
+
+        if (count_frame > 160)
+        {
+          if(count_cheat >= 5 ){
+            console.log("Gian lận");
+            count_over  = 0;
+            count_empty = 0;
+            count_cheat = 0;
+            handleStopCaptureClick();
+            status_recording = 0;
+
+          }
+          count_frame = 0;
+        }
+      }
     }
   };
 
@@ -53,6 +169,11 @@ function App() {
 
   return (
     <div className="App">
+
+
+    {recordedChunks.length > 0 && (
+            <button onClick={handleDownload}>Download</button>
+          )}
       <header className="App-header">
         <Webcam
           ref={webcamRef}
@@ -68,7 +189,7 @@ function App() {
             height: 480,
           }}
         />
-
+        
         <canvas
           ref={canvasRef}
           style={{
@@ -83,6 +204,7 @@ function App() {
             height: 480,
           }}
         />
+
       </header>
     </div>
   );
